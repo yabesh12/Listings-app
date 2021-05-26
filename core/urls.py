2123df36fa39edb917b1d4718ffab789b398298a
Yabesh <samyabeshv@gmail.com>
2021-05-26 17:23:20 +0530

    first commit

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+# core/urls.py
+from django.urls import path, include
+
+from .views import HomeView, CreateListing, ListingView
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('add-listing', CreateListing.as_view(), name='add_listing'),
+    path('listings/<slug:slug>/', ListingView.as_view(), name='listing'),
+]
